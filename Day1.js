bbf11001bea3107b989e8c9aa4788f592d9bad3f
Jotaniya  Darshit <167760417+Jotaniya-Darshit@users.noreply.github.com>
2025-01-06 16:04:17 +0530

    Create Server

--- Day1.js
+++ Day1.js
@@ -1,13 +1,13 @@
 const http = require('http');
-const port = 1009;
+const port = 2222;
 
-const portHandler = (req,res) => {
-    res.write("<h1>Hello.... Server is running</h1>");
+const Hello = (req,res) => {
+    res.write("<h1>Helloooo........ <br> Server is running </h1>");
     res.end();
 }
 
-const server = http.createServer(portHandler);
+const server = http.createServer(Hello);
 
 server.listen(port, (err) => {
-    err ? console.log(err ) : console.log("Server listening on port : " + port);
+    err ? console.log(err) : console.log(" Server running at port no. : " + port);
 })
